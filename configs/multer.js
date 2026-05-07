afeae430f724fb3947a974fe3d5658d9fa3707ec
Wasantha Jayasinghe <wasanthajayasinghe95@gmail.com>
2026-05-07 13:15:42 +0530

    fixed

--- configs/multer.js
+++ configs/multer.js
@@ -1,7 +1,3 @@
-import multer from "multer";
+import multer from 'multer';
 
-const storage = multer.memoryStorage();
-
-export const upload = multer({
-  storage,
-});
+export const upload = multer({storage: multer.diskStorage({})})
